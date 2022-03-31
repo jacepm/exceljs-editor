b468f6d7dc93ee15e283b78b6a96c990904fd6ba
Jc Mandawe <jcpmandawe@gmail.com>
2022-03-31 15:31:02 +0800

    feat: add read excel route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('excel')
+  async readExcel(@Query('file') file: string) {
+    return await this.appService.readExcel(file);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,49 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnprocessableEntityException,
+} from '@nestjs/common';
+import { Row, Workbook } from 'exceljs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async readExcel(file: string) {
+    try {
+      const workbook = new Workbook();
+      const readWorkbook = await workbook.xlsx.readFile(`assets/${file}`);
+      const worksheet = readWorkbook.getWorksheet(1);
+      const worksheetName = worksheet.name;
+      const firstRow = worksheet.getRow(1);
+      let data = [];
+
+      if (!firstRow.cellCount) return;
+
+      worksheet.eachRow(
+        { includeEmpty: true },
+        (row: Row, rowIndex: number) => {
+          if (rowIndex === 1) return;
+
+          const values = row.values;
+          const object = {};
+
+          for (let index = 1; index < firstRow.values.length; index++) {
+            object[firstRow.values[index]] = values[index] ?? '';
+          }
+
+          data = [...data, object];
+        },
+      );
+
+      const headers = Object.keys(data[0]);
+
+      return { worksheetName, headers, data };
+    } catch (error) {
+      if (error?.code === 'EBUSY') {
+        throw new UnprocessableEntityException(
+          'File is currently open or locked!',
+        );
+      }
+      throw new InternalServerErrorException('Error: Unable to process!');
+    }
   }
 }
